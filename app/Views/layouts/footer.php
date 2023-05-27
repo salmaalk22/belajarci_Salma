d918a7235bb717f1bb4b27fba7835613c6d41953
salmaalk22 <salmalaika22@gmail.com>
2023-05-27 11:55:12 +0700

    first commit

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer ">
+    <div class="container ">
+        <span class="text-muted ">Salma Khairiah Hadid.</span>
+    </div>
+</footer>
---
+++ app/Views/layoutsV2/header.php
@@ -0,0 +1,3 @@
+<h1 class="h3 mb-2 text-gray-800">Salma's Market</h1>
+<p class="mb-4">Welocome
+    <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
